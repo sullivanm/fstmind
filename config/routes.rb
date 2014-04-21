adad4682c3a689b26f367ecdfbe3e1d23bd215bd
fstech <fstech@fstech073.(none)>
2014-04-21 15:42:05 +0530

    added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Fstmind::Application.routes.draw do
+  devise_for :users
   get "home/index"
   root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
