cdaf638831ea208baf89b15235706df12715b9e4
sullivanm <sullivancoder@gmail.com>
2014-04-21 00:59:36 -0700

    Initail Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fstmind::Application.config.secret_key_base = '8da5a45522f397e082917bbd55836a6ed47f329a79939afe14830501635212dd2c96b7291e3136ea08ad82a5a216e28c915ebcbf20a88ddae44f1451f9803d55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fstmind::Application.config.session_store :cookie_store, key: '_fstmind_session'
